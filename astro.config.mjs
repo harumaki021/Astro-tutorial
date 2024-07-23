3188ee22ada5ce7ae0485db09f5c2774c3aa8caf
harumaki021 <harusamerain@gmail.com>
2024-07-23 10:14:16 +0900

    npx astro add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     hmr: true // Hot Module Replacement (ホットモジュールリプレースメント) を有効にします
   },
-  site: "https://example.com"
+  site: "https://example.com",
+  integrations: [preact()]
 });
